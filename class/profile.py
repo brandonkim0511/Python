91a8afdd6d99f4fbea9c8d73af8dffb6cf2e4db9
Brandon <brandonkim0511@gmail.com>
2019-07-08 16:58:57 +0900

    asd

--- class/profile.py
+++ class/profile.py
@@ -10,6 +10,10 @@
 name = input("What is your name? ")
 age = int(input("How old are you? "))
 grade = int(input("What grade are you in? "))
+gender = input("What gender are you? ")
+nationality = input("Where are you from? ")
+birthyear = int(input("When is your birth year? "))
+
 
 # gender
 #
@@ -21,15 +25,18 @@ grade = int(input("What grade are you in? "))
 
 
 resultForm = """
- ========result========
+ ========Result========
 name : {}
+gender : {}
 age 2019 : {}
 age 2020 : {}
 graduate at : {}
+nationality : {}
+40 years old in the year of : {}
 =========================
 """
 # print(resultForm.format(name,age,int(age) + 1,2019 + (12-int(grade)))
 
-print(resultForm.format(name, age, age + 1,2019 + (12-grade)))
+print(resultForm.format(name, gender, age, age + 1, 2019 + (12-grade), nationality, (40 - age) + birthyear))
 
-# HW : Develop this. 
+# HW : Develop this.
